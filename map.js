60fca93322170d803216ca3ede3ca40b958199bb
Garimiddi_Suman <sumangarimiddi@gmail.com>
2024-12-22 20:53:20 +0530

    added upto 30 in map

--- map.js
+++ map.js
@@ -1,26 +1,20 @@
 /*------------ 1) Squares of all Numbes in an array --------------*/
-const power = function (exponent) {
-  return function (base) {
-    return Math.pow(base, exponent);
-  };
-};
-
 const squaresOf = function (numbers) {
-  return numbers.map(power(2));
+  return numbers.map((num) => num * num);
 };
 
 console.log(squaresOf([1, 2, 3]));
 
 /*---------- 2) Lengths of each string in an array ---------------*/
 const lengthsOf = function (strings) {
-  return strings.map(function (string) { return string.length; });
+  return strings.map((string) => string.length);
 };
 
 console.log(lengthsOf(["apple", "banana", "kiwi"]));
 
 /*-------- 3) All Strings into UpperCase in an array --------------*/
 const uppercaseOf = function (strings) {
-  return strings.map(function (string) { return string.toUpperCase(); });
+  return strings.map((string) => string.toUpperCase());
 };
 
 console.log(uppercaseOf(["hello", "world"]));
@@ -28,7 +22,7 @@ console.log(uppercaseOf(["Suman", "garimiddi"]));
 
 /*------------- 4) First Chars of Each string --------------------*/
 const firstChar = function (string) {
-  return string === '' ? '' : string.at(0);
+  return string === '' ? string.slice(-1) : string.at(0);
 };
 
 const firstCharactersOf = function (strings) {
@@ -40,35 +34,21 @@ console.log(firstCharactersOf(["a", "", "k"]));
 
 /*------------- 5) Truth values of numbers ----------------------*/
 const truthValuesOf = function (numbers) {
-  return numbers.map(function (number) { return !(!number); });
+  return numbers.map((number) => number !== 0);
 };
 
 console.log(truthValuesOf([0, 1, 2, 3]));
 console.log(truthValuesOf([-1, 0, 2, 3]));
 
 /*------------- 6) Reverse Strings of Array ----------------------*/
-const reverseString = function (string) {
-  if (string === '') {
-    return '';
-  }
-
-  return string.slice(-1) + reverseString(string.slice(0, -1));
-};
-
 const reversedStringsOf = function (strings) {
-  return strings.map(reverseString);
+  return strings.map((string) => [...string].reverse().join(''));
 };
 
 console.log(reversedStringsOf(["hello", "world"]));
 console.log(reversedStringsOf(["", "a"]));
 
 /*------- 7) Double letters of each string in an array ---------------*/
-const repeat = function (times) {
-  return function (char) {
-    return char.repeat(times);
-  };
-};
-
 const doubleEveryChar = function (string) {
   return [...string].map((char) => char.repeat(2)).join('');
 };
@@ -81,63 +61,43 @@ console.log(doubleLettersOf(["cat", "dog", "bat"]));
 
 /*------------- 8) Complement of booleans ----------------------*/
 const negatedBooleansOf = function (booleans) {
-  return booleans.map(function (boolean) { return !boolean; });
+  return booleans.map((boolean) => !boolean);
 };
 
 console.log(negatedBooleansOf([true, false, true]));
 
 /*-------- 9) character codes according to UTF - 16 ----------------*/
 const charCodesOf = function (strings) {
-  return strings.map(function (char) { return char.charCodeAt(); });
+  return strings.map((char) => char.charCodeAt());
 };
 
 console.log(charCodesOf(["a", "b", "c"]));
 
 /*----------- 10) Extract domain names from mails ------------------*/
-const extractDomain = function (mail) {
-  const indexOfAtTheRate = mail.indexOf('@');
-
-  return mail.slice(indexOfAtTheRate + 1);
-};
-
 const domainNamesOf = function (emails) {
-  return emails.map(extractDomain);
+  return emails.map((email) => email.slice(email.indexOf('@') + 1));
 };
 
 console.log(domainNamesOf(["user1@gmail.com", "admin@yahoo.com"]));
 
 /*------------ 11) Split words into an array ---------------------*/
-const splitWordsIntoArray = function (string) {
-  return string.split(" ");
-};
-
 const splitWordsOf = function (strings) {
-  return strings.map(splitWordsIntoArray);
+  return strings.map((string) => string.split(" "));
 };
 
 console.log(splitWordsOf(["hello world", "goodbye moon"]));
 console.log(splitWordsOf(["suman", "suman garimiddi"]));
 
 /*-------------- 12) Join arrays into a String ---------------------*/
-const convertToString = function (array) {
-  return array.join("");
-};
-
 const joinedArraysOf = function (arrayOfArrays) {
-  return arrayOfArrays.map(convertToString);
+  return arrayOfArrays.map((array) => array.join(""));
 };
 
 console.log(joinedArraysOf([["a", "b"], ["c", "d"]]));
 
 /*---------------- 13) Repeated Strings -------------------------*/
-const repeatString = function (times) {
-  return function (string) {
-    return string.repeat(times);
-  };
-};
-
 const repeatedStringsOf = function (strings) {
-  return strings.map(repeatString(2));
+  return strings.map((string) => string + string);
 };
 
 console.log(repeatedStringsOf(["hi", "bye"]));
@@ -149,11 +109,8 @@ const isVowel = function (char) {
 };
 
 const vowelsCount = function (string) {
-  return string.split('').reduce(function (count, char) {
-    if (isVowel(char)) {
-      return count + 1;
-    }
-    return count;
+  return [...string].reduce(function (count, char) {
+    return isVowel(char) ? count + 1 : count;
   }, 0);
 };
 
@@ -165,52 +122,41 @@ console.log(countVowelsOf(["apple", "banana", "grape"]));
 
 /*--------------- 15) Reverse Array of Arrays -----------------------*/
 const reversedArraysOf = function (arrays) {
-  return arrays.map(function (array) { return array.reverse(); });
+  return arrays.map((array) => array.reverse());
 };
 
 console.log(reversedArraysOf([[1, 2, 3], [4, 5, 6]]));
 
 /*-------------- 16) Remove vowels from strings -----------------------*/
-// remove vowels from ["apple", "banana", "grape"] => ["ppl", "bnn", "grp"]
 const invert = function (fn) {
   return function (char) {
     return !fn(char);
   };
 };
 
-const removeVowels = function (string) {
-  const charsArray = string.split('');
-  return charsArray.filter(invert(isVowel)).join('');
-};
-
 const withoutVowelsOf = function (strings) {
-  return strings.map(removeVowels);
+  return strings.map((string) => [...string].filter(invert(isVowel)).join(''));
 };
 
 console.log(withoutVowelsOf(["apple", "banana", "grape"]));
 
 /*--------- 17) Cumulative Sums of array of arrays ------------------*/
 const sumUptoCurrentElement = function (array, element) {
-  const lastElement = array.at(-1);
+  const lastElement = array.at(-1) || 0;
   array.push(lastElement + element);
   return array;
 };
 
-const runningTotal = function (numbers) {
-  const nums = numbers.slice(1, numbers.length);
-  return nums.reduce(sumUptoCurrentElement, [numbers.at(0)]);
-};
-
 const cumulativeSumsOf = function (arrays) {
-  return arrays.map(runningTotal);
+  return arrays.map((numbers) => numbers.reduce(sumUptoCurrentElement, []));
 };
 
 console.log(cumulativeSumsOf([[1, 2, 3], [4, 5, 6]]));
 
-/*------------ 18) Reverse words in string --------------------------*/
+/*------------ 18) Reverse words in string ----------------------*/
 const reverseWord = function (string) {
   const arrayOfWords = string.split(" ");
-  return arrayOfWords.map(reverseString).join(' ');
+  return arrayOfWords.map((word) => [...word].reverse().join(''));
 };
 
 const reversedWordsOf = function (strings) {
@@ -221,7 +167,7 @@ console.log(reversedWordsOf(["hello world", "goodbye moon"]));
 
 /*-------- 19) Extract Unique Characters in string --------------*/
 // -->  Maintain the order of their first appearance in each string
-const isUnique = function (uniqueChars, char) {
+const concatUniqueChars = function (uniqueChars, char) {
   if (!uniqueChars.includes(char)) {
     uniqueChars.push(char);
   }
@@ -231,7 +177,7 @@ const isUnique = function (uniqueChars, char) {
 
 const extractUniqueChars = function (string) {
   const arrayOfChars = string.split('');
-  return arrayOfChars.reduce(isUnique, []).join('');
+  return arrayOfChars.reduce(concatUniqueChars, []).join('');
 };
 
 const uniqueCharactersOf = function (strings) {
@@ -242,7 +188,7 @@ console.log(uniqueCharactersOf(["apple", "banana", "grape"]));
 
 /*----------- 20) generate ranges from ------------------------*/
 const range = function (num) {
-  return Array(num).fill(0).map(function (_, i) { return i; });
+  return Array(num).fill(0).map((_, i) => i);
 };
 
 const rangesOf = function (numbers) {
@@ -251,7 +197,7 @@ const rangesOf = function (numbers) {
 
 console.log(rangesOf([3, 5, 2]));
 
-/*----------- 21) generate ranges from ------------------------*/
+/*----------- 21) capitalized First Letters ------------------------*/
 const firstCharCapital = function (string) {
   return string.at(0).toUpperCase() + string.slice(1);
 };
@@ -262,10 +208,10 @@ const capitalizedFirstLettersOf = function (strings) {
 
 console.log(capitalizedFirstLettersOf(["hello world", "goodbye moon"]));
 
-/*----------- 21) find Word Lengths ------------------------*/
+/*----------- 22) find Word Lengths ------------------------*/
 const findwordLength = function (string) {
   const arrayOfWords = string.split(' ');
-  return arrayOfWords.map(function (word) { return word.length; });
+  return arrayOfWords.map((word) => word.length);
 };
 
 const wordLengthsOf = function (strings) {
@@ -274,29 +220,21 @@ const wordLengthsOf = function (strings) {
 
 console.log(wordLengthsOf(["apple pie", "banana split"]));
 
-/*----------- 22) find Word Lengths ------------------------*/
-const flatArray = function (array) {
-  return array.flatMap(function (array) { return array; });
-};
-
+/*----------- 23) flat arrays in arrays ------------------------*/
 const flattenedArraysOf = function (arrays) {
-  return arrays.map(flatArray);
+  return arrays.map((array) => array.flat());
 };
 
 console.log(flattenedArraysOf([[1, [2, 3]], [4, [5, 6]]]));
 
-/*----------- 23) sort letters in array --------------------*/
-const sortString = function (string) {
-  return string.split('').sort().join('');
-};
-
+/*----------- 24) sort letters in array --------------------*/
 const sortedLettersOf = function (strings) {
-  return strings.map(sortString);
+  return strings.map((string) => [...string].sort().join(''));
 };
 
 console.log(sortedLettersOf(["cat", "bat", "rat"]));
 
-/*----------- 24) sort letters in array --------------------*/
+/*----------- 25) sort letters in array --------------------*/
 const wrapBrackets = function (string) {
   return '[' + string + ']';
 };
@@ -307,43 +245,56 @@ const wrappedStringsOf = function (strings) {
 
 console.log(wrappedStringsOf(["apple", "banana"]));
 
-/*----------- 25) calculate areas of rectangle -----------------*/
-const areaOfRectagle = function (rectangle) {
-  return rectangle.width * rectangle.height;
-};
-
+/*----------- 26) calculate areas of rectangle -----------------*/
 const calculateAreas = function (rectangles) {
-  return rectangles.map(areaOfRectagle);
+  return rectangles.map((rectangle) => rectangle.width * rectangle.height);
 };
 
-console.log(calculateAreas([{ width: 2, height: 3 }, { width: 4, height: 5 }]));
+console.log(calculateAreas([{ width: 2, height: 3 },
+{ width: 4, height: 5 }
+]));
 
-/*----------- 26) Extract Booleans -----------------*/
+/*----------- 27) Extract Booleans -----------------*/
 const extractFlags = function (objects) {
-  return objects.map(function (object) { return object.active; });
+  return objects.map((object) => object.active);
 };
 
 console.log(extractFlags([{ active: true }, { active: false }]));
 
-/*--- 26) calculate percentage contribution of each number -----*/
-
-// calculate percentage contribution of each number in [10, 20, 30] (relative to the total sum) => [16.67, 33.33, 50]
+/*----- 28) calculate percentage contribution of each number -----*/
 const add = function (x, y) {
   return x + y;
 };
 
-
 const percentageContributions = function (numbers) {
-  const sum = numbers.reduce(add, 0);
-  return numbers.map(contributionPercentage(sum));
+  const totalAmount = numbers.reduce(add, 0);
+  return numbers.map((contribution) => (contribution / totalAmount) * 100);
 };
 
-// subtract the smallest number from each number in [3, 8, 1] => [2, 7, 0]
-const subtractMin = function (numbers) { };
+console.log(percentageContributions([10, 20, 30]));
 
-// normalize strings by the longest string length in ["cat", "elephant", "dog"] => ["cat    ", "elephant", "dog    "]
-// (pad with spaces to match the longest length)
-const normalizeStringLengths = function (strings) { };
+/*----- 29) subtract the smallest number from each number -----*/
+const subtractMin = function (numbers) {
+  const small = numbers.reduce((num, small) => Math.min(num, small), Infinity);
+  return numbers.map((num) => num - small);
+};
+
+console.log(subtractMin([3, 8, 1]));
+
+/*----- 30) normalize strings by the longest string length -----*/
+const isLongestWord = function (longestWord, word) {
+  return longestWord.length < word.length ? word : longestWord;
+};
+
+const normalizeStringLengths = function (strings) {
+  const longestWordLength = strings.reduce(isLongestWord, '').length;
+
+  return strings.map((string) => string.padEnd(longestWordLength));
+};
+
+console.log(normalizeStringLengths(["cat", "elephant", "dog"]));
+
+
 
 // normalize strings by centering them based on the longest string length in ["cat", "elephant", "dog"] => ["  cat   ", "elephant", "  dog   "]
 // (pad with spaces to justify to the center)
